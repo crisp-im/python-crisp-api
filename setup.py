aefc590f87689d5ef8fbac90cb2363ac03b98f99
Valerian Saliou <valerian@valeriansaliou.name>
2018-01-04 10:29:34 +0100

    v1.0.1
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='crisp-api',
-    version='1.0.0',
+    version='1.0.1',
     author=u'Valerian Saliou',
     author_email='valerian@valeriansaliou.name',
     packages=find_packages(),
