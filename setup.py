1aca1cec3ab61579b042ba0b59d4be1fdb9a4d15
Eliott Vincent <dev@eliottvincent.com>
2021-03-23 12:35:04 +0100

    v1.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='crisp-api',
-    version='1.1.5',
+    version='1.1.6',
     author=u'Valerian Saliou',
     author_email='valerian@valeriansaliou.name',
     packages=find_packages(),
