d0b9774e7c31e464e06239f41f7724ecbdcc763c
Valerian Saliou <valerian@valeriansaliou.name>
2023-11-06 09:04:33 +0100

    v1.1.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='crisp-api',
-    version='1.1.18',
+    version='1.1.19',
     author=u'Valerian Saliou',
     author_email='valerian@valeriansaliou.name',
     packages=find_packages(),
